934e43c9311c0df20b234ae5db89bd3697d3f084
Luojin0418 <49770226+Luojin0418@users.noreply.github.com>
2019-11-01 16:13:56 +0800

    添加代码注释

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
 import requests
 import re
 import json
-import time
 from requests.exceptions import RequestException
 
 def get_one_page(url):
@@ -10,32 +9,30 @@ def get_one_page(url):
     }
     response = requests.get(url, headers=headers)
     if response.status_code == 200:
-        response.encoding = 'gbk'
+        response.encoding = 'gbk'#不设置的话，中文会出现乱码
         return response.text
     print('wrong connection')
     return None
 
 def write_to_file(content):
-    with open('11.5爬虫.txt', 'a', encoding='utf-8') as f:
+    with open('yourfilename.txt', 'a', encoding='utf-8') as f:
         f.write(content + '\n')
 
 def parse_one_page(html):
     pattern1 = re.compile('<div id="title">(.*?)</div>',re.S)
-    items1 = re.findall(pattern1, html)
+    items1 = re.findall(pattern1, html)#标题
     for i in items1:
         write_to_file(i.__str__())
     pattern = re.compile('&nbsp;&nbsp;&nbsp;&nbsp;(.*?)<br />',re.S)
-    items2 = re.findall(pattern, html)
+    items2 = re.findall(pattern, html)#内容
     for j in items2:
         write_to_file(j.__str__())
     write_to_file('------------------------------------------------------------------------------\n\n\n')
 
 def main(offset):
-    url = 'https://www.wenku8.net/novel/1/1973/'+str(offset)+'.htm'
+    url = 'https://www.wenku8.net/novel/1/1973/'+str(offset)+'.htm'#你的小说网址，自行更改
     html = get_one_page(url)
     parse_one_page(html)
 
-for i in range (101093, 101104):
+for i in range (101093, 101104):#你的小说哪一章到哪一张的网址，自行更改
     main(i)
-
-# this is a test
